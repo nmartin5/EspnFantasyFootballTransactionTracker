31b6dac9f71ad1e999d699c16dd8a5dd0b33ca7f
= <nmartin5@buffalo.edu>
2018-09-01 21:38:06 -0400

    Initial Commit

--- EspnFantasyFootballTransactionTracker/Model/ActivityCache.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace EspnFantasyFootballTransactionTracker.Model
-{
-    class ActivityCache
-    {
-        public List<ActivityItem> ActiviteItems { get; set; }
-    }
-}
